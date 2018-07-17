f5d9b6e7a7e4a84667060eaa189af4f9db6a6532
onlinepfz22 <1811603882@qq.com>
2018-07-17 18:52:19 +0800

    Initial commit

---
+++ activemq-domain/src/main/java/com/lwl/activemq/domain/News.java
@@ -0,0 +1,70 @@
+package com.lwl.activemq.domain;
+
+import java.io.Serializable;
+
+/**
+ * 新闻类信息
+ * @author Spring
+ *
+ * */
+public class News implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private long id;
+
+	private String title;
+
+	private String content;
+
+	private String url;
+
+	private String author;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	@Override
+	public String toString() {
+		return "News [id=" + id + ", title=" + title + ", content=" + content
+				+ ", url=" + url + ", author=" + author + "]";
+	}
+	
+}
---
+++ activemq-service/src/main/java/com/lwl/activemq/service/impl/ClientPushServiceImpl.java
@@ -0,0 +1,43 @@
+package com.lwl.activemq.service.impl;
+
+
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
+import org.springframework.stereotype.Service;
+
+import com.alibaba.fastjson.JSON;
+import com.lwl.activemq.domain.Client;
+import com.lwl.activemq.service.PushService;
+
+@Service("clientPushService")
+public class ClientPushServiceImpl implements PushService {
+	@Autowired
+	private JmsTemplate jmsTemplate;
+	
+	@Autowired
+	@Qualifier("clientServiceQueue")
+	private Destination destination;//注入推送给客户信息的队列
+	
+	@Override
+	public void push(final Object info) {
+		pushExecutor.execute(new Runnable() {
+			@Override
+			public void run() {
+				jmsTemplate.send(destination, new MessageCreator() {
+					public Message createMessage(Session session) throws JMSException {
+						Client p = (Client) info;
+						return session.createTextMessage(JSON.toJSONString(p));
+					}
+				});
+			}			
+		});
+	}
+
+}
